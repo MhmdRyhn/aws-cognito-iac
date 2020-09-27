af53d448094e1a66cc8fe367b0b17e1d5b013897
MhmdRyhn <rayhan.cse12@gmail.com>
2020-09-27 16:10:35 +0600

    Change in terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,25 +3,25 @@ terraform {
     // The bucket must be present in the account which one the profile indicates.
     // This bucket must allow the profile user to have the following permissions:
     // s3:ListBucket, s3:GetObject, s3:PutObject
-    // --> Sample policy for the terraform state bucket:
+    // --> Sample policy for the terraform-state-bucket:
     // {
     //  "Version": "2012-10-17",
     //  "Statement": [
     //    {
     //      "Effect": "Allow",
     //      "Action": "s3:ListBucket",
-    //      "Resource": "arn:aws:s3:::bucket"
+    //      "Resource": "arn:aws:s3:::terraform-state-bucket"
     //    },
     //    {
     //      "Effect": "Allow",
     //      "Action": ["s3:GetObject", "s3:PutObject"],
-    //      "Resource": "arn:aws:s3:::bucket/path/to/key"
+    //      "Resource": "arn:aws:s3:::terraform-state-bucket/*"
     //    }
     //  ]
     //}
 
     bucket  = "terraform-state-bucket"
-    key     = "deposit-front/deposit-front-state.tfstate"
+    key     = "application/application-state.tfstate"
     region  = "ap-southeast-1"
     profile = "dev"
   }
